827cd263f2826b99c5dcca1ccb2a1d3db8217fd2
rohanpaul93 <rohan.thekanath93@gmail.com>
2018-11-02 11:08:22 +0530

    Add files via upload

---
+++ keras_telco_churn.R
@@ -0,0 +1,74 @@
+pkgs <- c("keras", "lime", "tidyquant", "rsample", "recipes", "yardstick", "corrr")
+install.packages(pkgs)
+install.packages('tibble')
+
+install_keras()
+library(keras)
+library(lime)
+library(tidyquant)
+library(rsample)
+library(recipes)
+library(yardstick)
+library(corrr)
+
+churn_data_raw <- read_csv("WA_Fn-UseC_Telco_Customer_Churn.csv")
+glimpse(churn_data_raw)
+
+# Remove unnecessary data
+churn_data_tbl <- churn_data_raw %>%
+  select(-customerID) %>%
+  drop_na() %>%
+  select(Churn, everything())
+
+glimpse(churn_data_tbl)
+
+
+# Split test/training sets
+set.seed(100)
+train_test_split <- initial_split(churn_data_tbl, prop = 0.8)
+train_test_split
+
+# Retrieve train and test sets
+train_tbl <- training(train_test_split)
+test_tbl  <- testing(train_test_split)
+
+
+# Determine if log transformation improves correlation 
+# between TotalCharges and Churn
+train_tbl %>%
+  select(Churn, TotalCharges) %>%
+  mutate(
+    Churn = Churn %>% as.factor() %>% as.numeric(),
+    LogTotalCharges = log(TotalCharges)
+  ) %>%
+  correlate() %>%
+  focus(Churn) %>%
+  fashion()
+
+
+# Create recipe
+rec_obj <- recipe(Churn ~ ., data = train_tbl) %>%
+  step_discretize(tenure, options = list(cuts = 6)) %>%
+  step_log(TotalCharges) %>%
+  step_dummy(all_nominal(), -all_outcomes()) %>%
+  step_center(all_predictors(), -all_outcomes()) %>%
+  step_scale(all_predictors(), -all_outcomes()) %>%
+  prep(data = train_tbl)
+
+
+
+# Predictors
+x_train_tbl <- bake(rec_obj, newdata = train_tbl) %>% select(-Churn)
+x_test_tbl  <- bake(rec_obj, newdata = test_tbl) %>% select(-Churn)
+
+glimpse(x_train_tbl)
+
+# Response variables for training and testing sets
+y_train_vec <- ifelse(pull(train_tbl, Churn) == "Yes", 1, 0)
+y_test_vec  <- ifelse(pull(test_tbl, Churn) == "Yes", 1, 0)
+
+
+# Building our Artificial Neural Network
+model_keras <- keras_model_sequential()
+
+
